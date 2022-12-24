e532a013ec37c11fddc1a505837966ccf3f61ac7
DavidTrindade <dave.ferrari579@gmail.com>
2022-12-24 15:48:23 +0000

    Create server.js, starts server with middleware

---
+++ x-press-publishing/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const errorhandler = require('errorhandler');
+
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+app.use(bodyParser.json());
+app.use(errorhandler());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+module.exports = app;
