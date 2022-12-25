de832db7cd9e86c5aa14e4afae24b2adb589c6f3
DavidTrindade <dave.ferrari579@gmail.com>
2022-12-25 16:31:58 +0000

    Set up server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const errorhandler = require('errorhandler');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(errorhandler());
+app.use(cors());
+app.use(morgan('dev'));
+
+const apiRouter = require('./api/api');
+
+app.use('/api', apiRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+module.exports = app;
