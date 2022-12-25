70eaf7370c2c013a2087f1a8fafee69d058a99ba
DavidTrindade <dave.ferrari579@gmail.com>
2022-12-25 11:59:29 +0000

    Create artists router

--- x-press-publishing/api/api.js
+++ x-press-publishing/api/api.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const app = require('../server');
 
 const apiRouter = express.Router();
+
+const artistRouter = require('./artists');
+
+apiRouter.use('/artists', artistRouter);
 
 module.exports = apiRouter;
---
+++ x-press-publishing/api/artists.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const artistRouter = express.Router();
+
+module.exports = artistRouter;
