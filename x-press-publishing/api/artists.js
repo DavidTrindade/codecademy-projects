16b5004b4082ea561fc03e31b5c2f3cffa612e1f
DavidTrindade <dave.ferrari579@gmail.com>
2022-12-25 12:22:19 +0000

    Add get all endpoint

--- x-press-publishing/api/artists.js
+++ x-press-publishing/api/artists.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const sqlite3 = require('sqlite3');
 
 const artistRouter = express.Router();
+const db = new sqlite3.Database(
+    process.env.TEST_DATABASE || './database.sqlite'
+);
+
+artistRouter.get('/', (req, res, next) => {
+    db.all(
+        'SELECT * FROM Artist WHERE is_currently_employed = 1',
+        (err, rows) => {
+            if (err) {
+                return next(err);
+            }
+            res.send({ artists: rows });
+        }
+    );
+});
 
 module.exports = artistRouter;
